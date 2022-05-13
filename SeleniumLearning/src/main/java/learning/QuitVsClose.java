d4b1b626e025639d0a0e2723b92315a732608270
Kamalakannan Anandan <kamalakannan.anandan@gmail.com>
2022-05-13 14:00:25 -0500

    Quit VS Close code was updated.

--- SeleniumLearning/src/main/java/learning/QuitVsClose.java
+++ SeleniumLearning/src/main/java/learning/QuitVsClose.java
@@ -1,9 +1,27 @@
 package learning;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class QuitVsClose {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		
+		// Launch Chrome Browser
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+						
+		// Maximize the browser
+		driver.manage().window().maximize();
+						
+		//Open Leafground Alert Message page
+		driver.get("http://leafground.com/home.html");
+		
+		Thread.sleep(5000);
+		
+		driver.close();
+		
 
 	}
 
