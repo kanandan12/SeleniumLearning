d4aa27f44faf8f223b77a01cea39818f2c9353ee
Kamalakannan Anandan <kamalakannan.anandan@gmail.com>
2022-06-21 09:02:33 -0500

    small code changes

--- SeleniumLearning/src/main/java/learning/LastRowFetching.java
+++ SeleniumLearning/src/main/java/learning/LastRowFetching.java
@@ -3,7 +3,6 @@ package learning;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -28,12 +27,14 @@ public class LastRowFetching {
         int totalRows = rows.size();
         System.out.println("No of rows are: " +totalRows);
         
+        //No. of Columns
         List<WebElement> cols = driver.findElements(By.xpath("//*[@id='leftcontainer']/table/tbody/tr[1]/td"));
         int totalColumn = cols.size();
         System.out.println("No of columns are: "+totalColumn);
         
         String Lastrow="";
         
+        // Fetch the Last row data.
         for(int col=1; col<=totalColumn; col++) {
         	WebElement lastRowFetch = driver.findElement(By.xpath("//*[@id='leftcontainer']/table/tbody/tr["+totalRows+"]/td["+col+"]"));
             Lastrow = Lastrow + "\t" + lastRowFetch.getText();
